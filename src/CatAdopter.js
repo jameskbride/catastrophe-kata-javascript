c042a9c50297dcb8d76c3aca22daf3f570878b24
Jim Kirkbride <james.kirkbride@gmail.com>
2017-05-21 12:38:45 -0400

    Applying the neuter fee.

--- src/CatAdopter.js
+++ src/CatAdopter.js
@@ -3,12 +3,17 @@
 const ZERO_ADOPTION_FEE_COUNT = 21;
 const ADOPTION_FEE = 100;
 const SPAY_FEE = 150;
+const NEUTER_FEE = 75;
 
 class CatAdopter {
 
-    adopt(adoptionParams, adoptionFee = ADOPTION_FEE, spayFee = SPAY_FEE ) {
+    adopt(adoptionParams, adoptionFee = ADOPTION_FEE, spayFee = SPAY_FEE, neuterFee = NEUTER_FEE ) {
         let adoptionResults = this.applyAdoptionRules(adoptionParams, adoptionFee);
-        return {currentSavings: adoptionResults.currentSavings - spayFee};
+        if (adoptionParams.currentCatCount == 0 || adoptionParams.currentCatCount % 2 === 0) {
+            return {currentSavings: adoptionResults.currentSavings - spayFee};
+        } else {
+            return {currentSavings: adoptionResults.currentSavings - neuterFee};
+        }
     }
 
     applyAdoptionRules(adoptionParams, adoptionFee) {
@@ -19,4 +24,4 @@ class CatAdopter {
         }
     }
 }
-export {CatAdopter, ADOPTION_FEE, SPAY_FEE}
+export {CatAdopter, ADOPTION_FEE, SPAY_FEE, NEUTER_FEE}
--- test/CatAdopter.spec.js
+++ test/CatAdopter.spec.js
@@ -1,6 +1,6 @@
 'use strict';
 
-import {CatAdopter, ADOPTION_FEE, SPAY_FEE} from '../src/CatAdopter'
+import {CatAdopter, ADOPTION_FEE, SPAY_FEE, NEUTER_FEE} from '../src/CatAdopter'
 
 var chai = require('chai');
 var assert = chai.assert;
@@ -12,26 +12,33 @@ describe('CatAdopter', () => {
         var catAdopter;
         let SPAY_FEE_OVERRIDE = 0;
         let ADOPTION_FEE_OVERRIDE = 0;
+        let NEUTER_FEE_OVERRIDE = 0;
 
         beforeEach(() => {
             catAdopter = new CatAdopter();
         });
 
         it('should deduct a $100 one-time fee from the savings', () => {
-            let result = catAdopter.adopt({currentSavings: 100}, ADOPTION_FEE, SPAY_FEE_OVERRIDE);
+            let result = catAdopter.adopt({currentSavings: 100}, ADOPTION_FEE, SPAY_FEE_OVERRIDE, NEUTER_FEE_OVERRIDE);
             assert.equal(0, result.currentSavings);
         });
 
         it('should not deduct a $100 one-time fee for more than 20 cats', () => {
-            let result = catAdopter.adopt({currentSavings: 100, currentCatCount: 21}, ADOPTION_FEE, SPAY_FEE_OVERRIDE);
+            let result = catAdopter.adopt({currentSavings: 100, currentCatCount: 21}, ADOPTION_FEE, SPAY_FEE_OVERRIDE, NEUTER_FEE_OVERRIDE);
 
             assert.equal(100, result.currentSavings);
         });
 
-        it('should adopt female cats first with an applied spaying fee of $150', () => {
-            let result = catAdopter.adopt({currentSavings: 250, currentCatCount:0}, ADOPTION_FEE_OVERRIDE, SPAY_FEE);
+        it('should adopt alternating male and female cats with female cats first with an applied spaying fee of $150', () => {
+            let result = catAdopter.adopt({currentSavings: 150, currentCatCount:0}, ADOPTION_FEE_OVERRIDE, SPAY_FEE, NEUTER_FEE_OVERRIDE);
 
-            assert.equal(100, result.currentSavings);
+            assert.equal(0, result.currentSavings);
+        });
+
+        it('should adopt alternating male and female cats with male cats second with an applied neutering fee of $75', () => {
+            let result = catAdopter.adopt({currentSavings: 75, currentCatCount: 1}, ADOPTION_FEE_OVERRIDE, SPAY_FEE_OVERRIDE);
+
+            assert.equal(0, result.currentSavings);
         });
     });
 });
