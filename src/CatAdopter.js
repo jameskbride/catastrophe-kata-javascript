cdd94bcce8498f62cdbd184205343f156ecf8fa0
Jim Kirkbride <james.kirkbride@gmail.com>
2017-05-21 12:15:08 -0400

    Adding the spay fee.  Providing overrides to the fees for testing...

--- src/CatAdopter.js
+++ src/CatAdopter.js
@@ -2,15 +2,21 @@
 
 const ZERO_ADOPTION_FEE_COUNT = 21;
 const ADOPTION_FEE = 100;
+const SPAY_FEE = 150;
 
 class CatAdopter {
 
-    adopt(adoptionParams) {
+    adopt(adoptionParams, adoptionFee = ADOPTION_FEE, spayFee = SPAY_FEE ) {
+        let adoptionResults = this.applyAdoptionRules(adoptionParams, adoptionFee);
+        return {currentSavings: adoptionResults.currentSavings - spayFee};
+    }
+
+    applyAdoptionRules(adoptionParams, adoptionFee) {
         if (adoptionParams.currentCatCount >= ZERO_ADOPTION_FEE_COUNT) {
             return {currentSavings: adoptionParams.currentSavings}
         } else {
-            return {currentSavings: adoptionParams.currentSavings - ADOPTION_FEE};
+            return {currentSavings: adoptionParams.currentSavings - adoptionFee};
         }
     }
 }
-export {CatAdopter}
+export {CatAdopter, ADOPTION_FEE, SPAY_FEE}
--- test/CatAdopter.spec.js
+++ test/CatAdopter.spec.js
@@ -1,6 +1,6 @@
 'use strict';
 
-import  {CatAdopter} from '../src/CatAdopter'
+import {CatAdopter, ADOPTION_FEE, SPAY_FEE} from '../src/CatAdopter'
 
 var chai = require('chai');
 var assert = chai.assert;
@@ -10,18 +10,26 @@ describe('CatAdopter', () => {
     describe('Adoption Fees', () => {
 
         var catAdopter;
+        let SPAY_FEE_OVERRIDE = 0;
+        let ADOPTION_FEE_OVERRIDE = 0;
 
         beforeEach(() => {
             catAdopter = new CatAdopter();
         });
 
         it('should deduct a $100 one-time fee from the savings', () => {
-            let result = catAdopter.adopt({currentSavings: 100});
+            let result = catAdopter.adopt({currentSavings: 100}, ADOPTION_FEE, SPAY_FEE_OVERRIDE);
             assert.equal(0, result.currentSavings);
         });
 
         it('should not deduct a $100 one-time fee for more than 20 cats', () => {
-            let result = catAdopter.adopt({currentSavings: 100, currentCatCount: 21});
+            let result = catAdopter.adopt({currentSavings: 100, currentCatCount: 21}, ADOPTION_FEE, SPAY_FEE_OVERRIDE);
+
+            assert.equal(100, result.currentSavings);
+        });
+
+        it('should adopt female cats first with an applied spaying fee of $150', () => {
+            let result = catAdopter.adopt({currentSavings: 250, currentCatCount:0}, ADOPTION_FEE_OVERRIDE, SPAY_FEE);
 
             assert.equal(100, result.currentSavings);
         });
