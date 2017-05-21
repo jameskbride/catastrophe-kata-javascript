8547e7e91d5afdf0af3edf955f90255a83c749a5
Jim Kirkbride <james.kirkbride@gmail.com>
2017-05-21 11:45:33 -0400

    Refactoring.

--- src/CatAdopter.js
+++ src/CatAdopter.js
@@ -1,10 +1,15 @@
 'use strict';
+
+const ZERO_ADOPTION_FEE_COUNT = 21;
+const ADOPTION_FEE = 100;
+
 class CatAdopter {
+
     adopt(adoptionParams) {
-        if (adoptionParams.currentCatCount >= 21) {
+        if (adoptionParams.currentCatCount >= ZERO_ADOPTION_FEE_COUNT) {
             return {currentSavings: adoptionParams.currentSavings}
         } else {
-            return {currentSavings: adoptionParams.currentSavings - 100};
+            return {currentSavings: adoptionParams.currentSavings - ADOPTION_FEE};
         }
     }
 }
